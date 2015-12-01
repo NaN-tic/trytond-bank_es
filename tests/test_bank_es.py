e35914cbd7832ac6bdba450679cc5d5558ecbf2e
resteve <resteve@zikzakmedia.com>
2015-12-01 11:03:34 +0100

    Tests

--- tests/test_bank_es.py
+++ tests/test_bank_es.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part bank_es module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the bank_es module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class BankEsTestCase(unittest.TestCase):
-    'Test Spanish Bank module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('bank_es')
-
-    def test0005views(self):
-        'Test views'
-        test_view('bank_es')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class BankEsTestCase(ModuleTestCase):
+    'Test Bank Es module'
+    module = 'bank_es'
 
 
 def suite():
